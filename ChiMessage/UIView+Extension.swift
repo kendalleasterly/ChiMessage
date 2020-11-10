00b99b6702cf6bd969c5dec6aee035553369b01e
Kendall Easterly <qfletcher89@gmail.com>
2020-11-10 09:24:35 -0600

    added special view extension for iOS platform

---
+++ ChiMessage/UIView+Extension.swift
@@ -0,0 +1,28 @@
+//
+//  UIView+Extension.swift
+//  ChiMessage
+//
+//  Created by Kendall Easterly on 11/7/20.
+//
+
+import SwiftUI
+
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
+
+struct Blur: UIViewRepresentable {
+    var style: UIBlurEffect.Style = .systemChromeMaterial
+    
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        return UIVisualEffectView(effect: UIBlurEffect(style: style))
+    }
+    
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        uiView.effect = UIBlurEffect(style: style)
+    }
+}
